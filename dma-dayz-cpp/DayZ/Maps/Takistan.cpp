c1b89c04a668f993ba9cd8435561ea9ce07c32db
SoTMaulder <158225586+SoTMaulder@users.noreply.github.com>
2025-03-24 13:04:01 -0400

    had to fix something minor
    had to fix something minor

--- dma-dayz-cpp/DayZ/Maps/Takistan.cpp
+++ dma-dayz-cpp/DayZ/Maps/Takistan.cpp
@@ -2,5 +2,5 @@
 
 ImVec2 DayZ::Takistan::translatePoint(const DMARender::Vector3& gamePoint)
 {
-    return ImVec2(gamePoint.x, 12736.0f - gamePoint.z);
+    return ImVec2(gamePoint.x, 12800.0f - gamePoint.z);
 }
